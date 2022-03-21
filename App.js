520e9212006984e193959dfef750c70059ad1f5b
doni lima <danadoni.lima@gmail.com>
2022-03-21 13:21:43 -0300

    primeira versão com cabeçalho concluido e em componente

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import { StyleSheet, Text, View } from "react-native";
+import Cabecalho from "./src/components/cabecalho";
+
+
+export default function App() {
+  return (
+    <View style={{backgroundColor:'#D3D3D3', flex:1}}>
+      <Cabecalho></Cabecalho>
+
+     </View>
+  );
+}
